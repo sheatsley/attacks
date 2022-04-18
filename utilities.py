fbbe7fbcd0a2f2078f5d037307e8729a67eec6e8
Ryan Sheatsley <ryan@sheatsley.me>
2022-04-18 17:43:02 -0400

    migrate utilities from datasets

---
+++ utilities.py
@@ -0,0 +1,29 @@
+"""
+The utilities module defines functions used throughout CleverTorch.
+Author: Ryan Sheatsley
+Mon Apr 18 2022
+"""
+import builtins  # Built-in objects
+import time  # Time access and conversions
+
+
+def print(*args, **kwargs):
+    """
+    This function wraps the print function, prepended with a timestamp.
+
+    :param *args: positional arguments supported by print()
+    :type *args: tuple
+    :param **kwargs: keyword arguments supported by print()
+    :type **kwargs: dictionary
+    :return: None
+    :rtype: NoneType
+    """
+    return builtins.print(f"[{time.asctime()}]", *args, **kwargs)
+
+
+if __name__ == "__main__":
+    """
+    This runs some basic unit tests with the functions defined in this module
+    """
+    print("Test string with implicit date.")
+    raise SystemExit(0)
