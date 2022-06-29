1f692785ea367b15901eed7e0ce26986bf362c52
Ryan Sheatsley <ryan@sheatsley.me>
2022-06-29 16:53:14 -0400

    move instance variables to class variables and provide complete implementation of SGD and Adam

--- optimizers.py
+++ optimizers.py
@@ -14,27 +14,32 @@ class SGD(torch.optim.SGD):
     """
     This class is identical to the SGD class in PyTorch, with the exception
     that two additional attributes are added: req_loss (set to False) and
-    req_acc (set to False). Unlike SGD, some optimizers (e.g., MBS & BWSGD)
-    either require the current model loss or accuracy to perform their update
+    req_acc (set to False). Unlike SGD, some optimizers (e.g., MBS & BWSGD) can
+    require the adversarial loss or model accuracy to perform their update
     step, and thus, we instantiate these parameters for a homogenous interface.
 
     :func:`__init__` instantiates an SGD object
     """
 
-    def __init__(self, **kwargs):
+    req_acc = False
+    req_loss = False
+
+    def __init__(self, atk_loss=None, model_acc=None, **kwargs):
         """
         This method instantiates an SGD object. It accepts keyword arguments
         for the PyTorch parent class described in:
         https://pytorch.org/docs/stable/generated/torch.optim.SGD.html.
 
+        :param atk_loss: returns the current loss of the attack
+        :type atk_loss: Loss object
+        :param model_acc: returns the current model accuracy
+        :type model_acc: ScikitTorch Model instance method
         :param kwargs: keyword arguments for torch.optim.SGD
         :type kwargs: dict
         :return Stochastic Gradient Descent optimizer
         :rtype: SGD object
         """
         super().__init__(**kwargs)
-        self.req_loss = False
-        self.req_acc = False
         return None
 
 
@@ -43,11 +48,14 @@ class Adam(torch.optim.Adam):
     This class is identical to the Adam class in PyTorch, with the exception
     that two additional attributes are added: req_loss (set to False) and
     req_acc (set to False). Unlike Adam, some optimizers (e.g., MBS & BWSGD)
-    either require the current model loss or accuracy to perform their update
+    require the adversarial loss or model accuracy to perform their update
     step, and thus, we instantiate these parameters for a homogenous interface.
 
     :func:`__init__` instantiates an Adam object
     """
+
+    req_acc = False
+    req_loss = False
 
     def __init__(self, **kwargs):
         """
@@ -61,6 +69,4 @@ class Adam(torch.optim.Adam):
         :rtype: Adam object
         """
         super().__init__(**kwargs)
-        self.req_loss = False
-        self.req_acc = False
         return None
