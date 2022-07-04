0c8de62f030b809feadc226831d91ebb9833818a
Ryan Sheatsley <ryan@sheatsley.me>
2022-07-04 15:53:33 -0400

    update traveler docs and move tanh map to be a utility function in traveler module (does not require any state)

--- traveler.py
+++ traveler.py
@@ -12,16 +12,15 @@ import torch  # Tensors and Dynamic neural networks in Python with strong GPU ac
 class Traveler:
     """
     The Traveler class handles consuming gradient information from inputs and
-    applying perturbations appropriately, as shown in [paper_url]. Under the
-    hood, Traveler objects serve as intelligent wrappers for PyTorch optimizers
-    and the methods defined within this class are designed to facilitate
-    crafting adversarial examples.
+    applying perturbations appropriately. Under the hood, Traveler objects
+    serve as intelligent wrappers for PyTorch optimizers and the methods
+    defined within this class are designed to facilitate crafting adversarial
+    examples.
 
     :func:`__init__`: instantiates Traveler objects
     :func:`__call__`: performs one step of input manipulation
     :func:`__repr__`: returns Traveler parameter values
     :func:`initialize`: prepares Traveler objects to operate over inputs
-    :func:`tanh_space`: maps a tensor into (and out of) tanh-space
     """
 
     def __init__(self, change_of_variables, optimizer, random_restart, closure=()):
@@ -29,9 +28,9 @@ class Traveler:
         This method instantiates Traveler objects with a variety of attributes
         necessary for the remaining methods in this class. Conceptually,
         Travelers are responsible for applying a perturbation to an input based
-        on some gradient information, and thus the following attributes are
+        on some gradient information, and thus, the following attributes are
         collected: (1) whether change of variables is applied (i.e., mapping
-        into and out of the hyperbolic tangent space), (2) an PyTorch-based
+        into and out of the hyperbolic tangent space), (2) a PyTorch-based
         optimizer object from the optimizer module (which is initialized with
         α, the learning rate), (3) the minimum and maximum values to initialize
         inputs (i.e., random restart, often sampled between -ε and ε ), and (5)
@@ -55,9 +54,24 @@ class Traveler:
         self.params = {
             "α": optimizer.lr,
             "CoV": change_of_variables,
-            "optim": optimizer.__name__,
+            "optim": type(optimizer).__name__,
             "RR": (-random_restart, random_restart),
         }
+        return None
+
+    def __call__(self):
+        """
+        This method is the heart of Traveler objects. It performs two
+        functions: (1) to apply a single optimization step (via Optimizer
+        objects), and (2) calls closure subroutines before returning. Notably,
+        this method assumes that the gradients associated with leaf variables
+        attached to optimizers is populated.
+
+        :return: None
+        :rtype: NoneType
+        """
+        self.optimizer.step()
+        [f() for f in self.closure]
         return None
 
     def __repr__(self):
@@ -69,21 +83,6 @@ class Traveler:
         :rtype: str
         """
         return f"Traveler({self.params})"
-
-    def __call__(self):
-        """
-        This method is the heart of Traveler objects. It performs two
-        functions: (1) to perform a single optimization step (via Optimizer
-        objects), and (2) calls closure subroutines before returning. Notably,
-        this method assumes that the gradients associated with leaf variables
-        attached to optimizers is populated.
-
-        :return: None
-        :rtype: NoneType
-        """
-        self.optimizer.step()
-        [f() for f in self.closure]
-        return None
 
     def initialize(self, x, p):
         """
@@ -114,48 +113,48 @@ class Traveler:
         # subroutine (2): change of variables
         if self.change_of_variables:
             print(f"Applying change of variables to {len(x)} samples...")
-            self.tanh_space(x, True)
+            tanh_space(x, True)
 
         # final subroutine: attach p to the optimizer
         print(f"Attaching the perturbation vector to {self.optmizer.__name__}...")
         self.optimizer.add_param_group({"params": p})
         return None
 
-    def tanh_space(self, x, into=False):
-        """
-        This method maps x into the tanh-space, as shown in
-        https://arxiv.org/pdf/1608.04644.pdf. Specifically, the transformation
-        is defined as follows:
 
-                            w = ArcTanh(x * 2 - 1)                          (1)
+def tanh_space(x, into=False):
+    """
+    This method maps x into the tanh-space, as shown in
+    https://arxiv.org/pdf/1608.04644.pdf. Specifically, the transformation
+    is defined as follows:
 
-        where w is x in tanh-space, x is the original input, and Δ is the
-        resultant perturbation to be added to x to produce the adversarial
-        example (i.e., the variable we optimize over). Upon initialization, we
-        first map x into the tanh space as shown in (1), and subsequently, when
-        optimizing for Δ, we map x back out of the tanh space via:
+                        w = ArcTanh(x * 2 - 1)                          (1)
 
-                            x = (Tanh(w + Δ) + 1) / 2                       (2)
+    where w is x in tanh-space, x is the original input, and Δ is the
+    resultant perturbation to be added to x to produce the adversarial
+    example (i.e., the variable we optimize over). Upon initialization, we
+    first map x into the tanh space as shown in (1), and subsequently, when
+    optimizing for Δ, we map x back out of the tanh space via:
 
-        where Δ is the computed perturbation to produce an adversarail
-        examples. As described in https://arxiv.org/pdf/1608.04644.pdf, (2)
-        ensures that x is gauranteed to be within the range [0, 1] (thus
-        avoiding any clipping mechanisms). Whether (1) or (2) is applied is
-        determined by the inverse argument, and, importantly, this method
-        operates in-place, as x often needs to be attached to the optimizer.
+                        x = (Tanh(w + Δ) + 1) / 2                       (2)
 
-        :param x: the batch of inputs to map into tanh-space
-        :type x: PyTorch FloatTensor object (n, m)
-        :param into: whether to map into (or out of) the tanh-space
-        :type into: boolean
-        :return: x mapped into (or back out of) tanh-space
-        :rtype: PyTorch FloatTensor object (n, m)
-        """
-        return (
-            x.mul_(2).sub_(1).arctanh_(1 - torch.finfo(x.dtype).eps)
-            if into
-            else x.tanh_().add_(1).div_(2)
-        )
+    where Δ is the computed perturbation to produce an adversarail examples. As
+    described in https://arxiv.org/pdf/1608.04644.pdf, (2) ensures that x is
+    gauranteed to be within the range [0, 1] (thus avoiding any clipping
+    mechanisms). Whether (1) or (2) is applied is determined by the inverse
+    argument.
+
+    :param x: the batch of inputs to map into tanh-space
+    :type x: PyTorch FloatTensor object (n, m)
+    :param into: whether to map into (or out of) the tanh-space
+    :type into: boolean
+    :return: x mapped into (or back out of) tanh-space
+    :rtype: PyTorch FloatTensor object (n, m)
+    """
+    return (
+        x.mul_(2).sub_(1).arctanh_(1 - torch.finfo(x.dtype).eps)
+        if into
+        else x.tanh_().add_(1).div_(2)
+    )
 
 
 if __name__ == "__main__":
