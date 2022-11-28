99e336b37b683c32c18ad32137cdd376686ddb57
Ryan Sheatsley <ryan@sheatsley.me>
2022-11-28 17:15:05 -0600

    add initial unittest

---
+++ tests.py
@@ -0,0 +1,13 @@
+"""
+This module runs attack performance tests against other AML libraries.
+Authors: Ryan Sheatsley and Blaine Hoak
+Mon Nov 28 2022
+"""
+
+import clevertorch  # ML robustness evaluations with PyTorch
+import unittest  # Unit testing framework
+
+class 
+
+if __name__ == "__main__":
+    raise SystemExit(0)
