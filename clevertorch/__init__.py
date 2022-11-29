98229f1eeb1b7539ed24522bf6a4d4a2634027f7
Ryan Sheatsley <ryan@sheatsley.me>
2022-11-29 17:04:28 -0600

    add docs

--- clevertorch/__init__.py
+++ clevertorch/__init__.py
@@ -1,7 +1,12 @@
-import clevertorch.attacks
-import clevertorch.loss
-import clevertorch.optimizer
-import clevertorch.saliency
-import clevertorch.surface
-import clevertorch.traveler
-import clevertorch.utilities
+"""
+This module initializes CleverTorch.
+Author: Ryan Sheatsley and Blaine Hoak
+Tue Nov 29 2022
+"""
+import clevertorch.attacks  # pytorch-based aml attacks
+import clevertorch.loss  # pytorch-based loss functions popular in aml
+import clevertorch.optimizer  # pytorch-based optimizers popular in aml
+import clevertorch.saliency  # pytorch-based saliency maps popular in aml
+import clevertorch.surface  # gradient computation for aml attacks in pytorch
+import clevertorch.traveler  # perturbation computations for aml attacks in pytorch
+import clevertorch.utilities  # miscelleanous utility functions
