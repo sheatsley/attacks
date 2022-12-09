9c9a983ad9e522da22bd9bce77b7c3b7789d213e
Ryan Sheatsley <ryan@sheatsley.me>
2022-12-09 17:46:29 -0600

    switch to aml api

--- aml/__init__.py
+++ aml/__init__.py
@@ -1,12 +1,11 @@
 """
-This module initializes CleverTorch.
+This module initializes the adversarial machine learning repo.
 Author: Ryan Sheatsley and Blaine Hoak
 Tue Nov 29 2022
 """
-import clevertorch.attacks  # pytorch-based aml attacks
-import clevertorch.loss  # pytorch-based loss functions popular in aml
-import clevertorch.optimizer  # pytorch-based optimizers popular in aml
-import clevertorch.saliency  # pytorch-based saliency maps popular in aml
-import clevertorch.surface  # gradient computation for aml attacks in pytorch
-import clevertorch.traveler  # perturbation computations for aml attacks in pytorch
-import clevertorch.utilities  # miscelleanous utility functions
+import aml.attacks  # pytorch-based aml attacks
+import aml.loss  # pytorch-based loss functions popular in aml
+import aml.optimizer  # pytorch-based optimizers popular in aml
+import aml.saliency  # pytorch-based saliency maps popular in aml
+import aml.surface  # gradient computation for aml attacks in pytorch
+import aml.traveler  # perturbation computations for aml attacks in pytorch
