8d5db33f57fe5240af9d9689a7d3ec10ff24633f
Ryan Sheatsley <ryan@sheatsley.me>
2023-01-24 21:32:17 -0600

    add new shrinking norm, identity, and max norm random start strategies (currently debugging, this commit will be cleaned up)

--- aml/traveler.py
+++ aml/traveler.py
@@ -39,8 +39,8 @@ class Traveler:
         :type change_of_variables: bool
         :param optimizer: optimization algorithm to use
         :type optimizer: optimizer module object
-        :param random_start: magnitude of a random perturbation
-        :type random_start: scalar
+        :param random_start: desired random start heuristic
+        :type random_start: callable
         :param closure: subroutines to run at the end of __call__
         :type closure: tuple of callables
         :return: a traveler
@@ -58,8 +58,9 @@ class Traveler:
             "α": optimizer.defaults.get("lr", "adaptive"),
             "CoV": change_of_variables,
             "optim": type(optimizer).__name__,
-            "RR": (-random_start, random_start),
+            "RS": random_start.__name__,
         }
+        self.state = False
         return None
 
     def __call__(self):
@@ -87,31 +88,35 @@ class Traveler:
         """
         return f"Traveler({self.params})"
 
-    def initialize(self, x, p):
+    def initialize(self, x, p, b, norm, eps):
         """
         This method performs any preprocessing and initialization steps prior
         to crafting adversarial examples. Specifically, some attacks (1)
-        initialize perturbation vectors via a random perturbation (e.g., PGD),
-        or (2) apply change of variables (e.g. CW) to the original inputs which
-        requires that maximum values be less than the minimum value mapped to
-        infinity by arctanh (i.e., 1-machine epsilon). Finally, p is attached
-        to the optimizer (by reinstantiation, given that PyTorch optimizers
-        cannot be initialized without a parameter group).
+        initialize perturbation vectors via a random perturbation (e.g., PGD &
+        FAB), or (2) apply change of variables (e.g. CW) to the original inputs
+        which requires that maximum values be less than the minimum value
+        mapped to infinity by arctanh (i.e., 1-machine epsilon). Finally, p is
+        attached to the optimizer (by reinstantiation, given that PyTorch
+        optimizers cannot be initialized without a parameter group).
 
         :param x: the batch of inputs to produce adversarial examples from
         :type x: torch Tensor object (n, m)
         :param p: the perturbation vectors used to craft adversarial examples
         :type p: torch Tensor object (n, m)
+        :param b: the best perturbation vectors seen thus far
+        :type b: torch Tensor object (n, m)
+        :param norm: lp norm of the attack
+        :type norm: float; 0, 2, or inf
+        :param epsilon: maximum allowable distortion
+        :type epsilon: float or int if l0
         :return: None
         :rtype: NoneType
         """
 
         # subroutine (1): random start
-        p.add_(
-            torch.distributions.uniform.Uniform(
-                -self.random_start, self.random_start
-            ).sample(p.size())
-        ) if self.random_start else 0
+        if self.state == True:
+            self.random_start(p, b=b, norm=norm, epsilon=eps)
+            pass
 
         # subroutine (2): change of variables
         if self.change_of_variables:
@@ -122,11 +127,88 @@ class Traveler:
         return None
 
 
+def identity_start(p, **kwargs):
+    """
+    This method is an identity function for random start startegies. It returns
+    the input as-is. Keyword arguments are accepted to provide a homogeneous
+    interface across random start strategies.
+
+    :param p: the perturbation vectors used to craft adversarial examples
+    :type p: torch Tensor object (n, m)
+    :return: the perturbation vector used to craft adversarial examples
+    :rtype: torch Tensor object (n, m)
+    """
+    return p
+
+
+def max_start(p, norm, epsilon, **kwargs):
+    """
+    This function randomly perturbs inputs, made popular by PGD
+    (https://arxiv.org/pdf/1706.06083.pdf). The behavior of the random
+    perturbation is determined by norm: (1) for l0 attacks, a set of values are
+    uniformly sampled from [-1, 1], according to the threat model, (2) for l2
+    attacks, values are sampled from a normal distribution and renormed to the
+    threat model, and (3) for l∞ attacks, values are sampled uniformly from ±ε.
+    Keyword arguments are accepted to provide a homogeneous interface across
+    random start strategies.
+
+    :param p: the perturbation vectors used to craft adversarial examples
+    :type p: torch Tensor object (n, m)
+    :param norm: lp norm of the attack
+    :type norm: float; 0, 2, or inf
+    :param epsilon: maximum allowable distortion (per input)
+    :type epsilon: torch Tensor object (n,)
+    :return: randomly perturbed vectors
+    :rtype: torch Tensor object (n, m)
+    """
+
+    # sample from uniform, select up to l0, and permute indices
+    breakpoint()
+    if norm == 0:
+        p.uniform_(-1, 1)
+        shuffle = torch.randint(p.size(1), p.size()).argsort()
+        keep = torch.arange(1, p.size(1) + 1).repeat(p.size(0), 1).add(epsilon)
+        return p.scatter_(1, shuffle, p.where(keep > p.size(1), torch.tensor(0)))
+    elif norm == 2:
+        return p.normal_().renorm_(2, 0, 1).mul_(epsilon)
+    elif norm == torch.inf:
+        return p.uniform_(-1, 1).clamp_(-epsilon, epsilon)
+    else:
+        raise NotImplementedError
+
+
+def shrinking_start(p, b, norm, epsilon, **kwargs):
+    """
+    This function randomly perturbs inputs based on the norm of the best
+    adversarial example seen thus far, made popular by FAB
+    (https://arxiv.org/pdf/1907.02044.pdf). Specifically, this function sets
+    epsilon to be the minimum of itself and the smallest norm seen thus far,
+    divides the result by two, and calls the max_norm function. Keyword
+    arguments are accepted to provide a homogeneous interface across random
+    start strategies.
+
+    :param p: the perturbation vectors used to craft adversarial examples
+    :type p: torch Tensor object (n, m)
+    :param b: the best perturbation vectors seen thus far
+    :type b: torch Tensor object (n, m)
+    :param norm: lp norm of the attack
+    :type norm: float; 0, 2, or inf
+    :param epsilon: maximum allowable distortion
+    :type epsilon: float or int if l0
+    :return: randomly perturbed vectors
+    :rtype: torch Tensor object (n, m)
+    """
+    breakpoint()
+    b_norm = b.norm(norm, 1, keepdim=True)
+    epsilon = b_norm.where(b_norm < epsilon, torch.tensor(epsilon)).div(2)
+    return max_start(p, norm, epsilon.int() if norm == 0 else epsilon)
+
+
 def tanh_space(x, into=False):
     """
-    This method maps x into the tanh-space, as shown in
-    https://arxiv.org/pdf/1608.04644.pdf. Specifically, the transformation
-    is defined as follows:
+    This function maps x into the tanh-space, as shown in
+    https://arxiv.org/pdf/1608.04644.pdf. Specifically, the transformation is
+    defined as follows:
 
                         w = ArcTanh(x * 2 - 1)                          (1)
 
@@ -152,25 +234,20 @@ def tanh_space(x, into=False):
     :rtype: torch Tensor object (n, m)
     """
     eps = 1 - torch.finfo(x.dtype).eps
-    return (
-        x.mul_(2).sub_(1).mul_(eps).arctanh_()
-        if into
-        else x.tanh().add(eps).div(2 * eps)
-    )
+    return x.mul_(2).sub_(1).mul_(eps).arctanh_() if into else x.tanh().add(1).div(2)
 
 
 def tanh_space_p(x, p, into=False):
     """
-    This method extracts the perturbation vector when using change of
-    variables. When using the technique, the computed perturbation is in
-    the tanh-space (which is convenient when returning adversarial examples
-    directly). To properly compute progress statistics as well as
-    projecting onto l2-norm balls (parameterized by epsilon), this method
-    extracts the perturbation vector in the real space by first mapping the
-    current input out of the tanh-space and subtracting it from the
-    original input. Moreover, to facilitate mapping perturbations into the
-    tanh-space, this also supports an out-of-place-non-scaled version of
-    the procedure defined in tanh_map.
+    This function extracts the perturbation vector when using change of
+    variables. When using the technique, the computed perturbation is in the
+    tanh-space (which is convenient when returning adversarial examples
+    directly). To properly compute progress statistics as well as projecting
+    onto l2-norm balls (parameterized by epsilon), this method extracts the
+    perturbation vector in the real space by first mapping the current input
+    out of the tanh-space and subtracting it from the original input. Moreover,
+    to facilitate mapping perturbations into the tanh-space, this also supports
+    an out-of-place-non-scaled version of the procedure defined in tanh_map.
 
     :param x: initial inputs in tanh-space
     :type x: torch Tensor object (n, m)
