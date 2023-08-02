41e00ad73930d7c7e2a17162418a2c0a9ff9fb78
Ryan Sheatsley <ryan@sheatsley.me>
2023-08-02 16:58:41 -0500

    always generate version file

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 """
 Build script for Adversarial Machine Learning (aml) library.
 """
-import setuptools  # Easily download, build, install, upgrade, and uninstall Python packages
+import subprocess
+
+import setuptools
+
+# compute git hash and save to file for non-editable installs
+# overriding install for package data is bugged: https://github.com/pypa/setuptools/issues/1064
+version = subprocess.check_output(
+    ("git", "rev-parse", "--short", "HEAD"), text=True
+).strip()
+with open("aml/VERSION", "w") as f:
+    f.write(f"{version}\n")
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -25,6 +35,7 @@ setuptools.setup(
     keywords="machine-learning pytorch adversarial-machine-learning",
     name="aml",
     packages=setuptools.find_packages(),
+    package_data={"aml": ["VERSION"]},
     python_requires=">=3.10",
     url="https://github.com/sheatsley/attacks",
     version="1.1",
