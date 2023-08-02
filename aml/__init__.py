aa0a2c98d0607e8b7630ca137dcdcdf58b739206
Ryan Sheatsley <ryan@sheatsley.me>
2023-08-02 16:59:13 -0500

    adapt version computation to load static version file for non-dev installs

--- aml/__init__.py
+++ aml/__init__.py
@@ -1,6 +1,7 @@
 """
 This module initializes the adversarial machine learning repo.
 """
+import pathlib
 import subprocess
 
 from aml.attacks import (
@@ -28,6 +29,11 @@ __all__ = (
     "jsma",
     "pgd",
 )
-__version__ = subprocess.check_output(
-    ("git", "-C", *__path__, "rev-parse", "--short", "HEAD"), text=True
-).strip()
+try:
+    cmd = ("git", "-C", *__path__, "rev-parse", "--short", "HEAD")
+    __version__ = subprocess.check_output(
+        cmd, stderr=subprocess.DEVNULL, text=True
+    ).strip()
+except subprocess.CalledProcessError:
+    with open(pathlib.Path(__file__).parent / "VERSION", "r") as f:
+        __version__ = f.read().strip()
